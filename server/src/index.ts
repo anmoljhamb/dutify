cde11fcc94ceb5834c40a19ef2635233364e840d
Anmol Jhamb <talktoanmol@outlook.com>
2023-07-22 17:02:18 +0530

    added basic error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,34 @@
-import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import createError, { HttpError } from "http-errors";
+import morgan from "morgan";
+import path from "path";
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-    console.log("workng...");
-    return res.status(200).json({ message: "working..." });
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 404 not found.
+app.use((req, _res, next) => {
+    const err = new createError.NotFound(
+        `The requested url ${req.url} was not found.`
+    );
+    return next(err);
 });
 
-app.listen(8080, () => {
-    console.log(`Listening on the url ${8080}`);
+// Error Handling
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    if (err instanceof HttpError) {
+        return res.status(err.statusCode).json({ message: err.message });
+    }
+    return res.status(500).json({ err });
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on the url *:${PORT}`);
 });
