e87619e9794b6195b1658d3cfb4de64d274c8c9e
Anmol Jhamb <talktoanmol@outlook.com>
2023-07-23 17:25:35 +0530

    theme implemented for tailwind css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@mui/material";
+import * as Constants from "./constants";
 
 const App = () => {
+    console.log(Constants);
+
     return (
-        <div className="justitfy-center flex h-screen w-screen flex-col items-center justify-center bg-black">
+        <div className="flex h-screen w-screen flex-col items-center justify-center bg-bgColor">
             <h1 className="text-4xl uppercase text-white">hello world!</h1>
             <Button>Login???</Button>
         </div>
---
+++ client/src/constants.ts
@@ -0,0 +1,10 @@
+import colors from "./variables.module.scss";
+
+export const COLOR_PRIMARY = colors.primary;
+export const COLOR_SECONDARY = colors.secondary;
+export const COLOR_BG = colors.bg;
+export const COLOR_TEXT = colors.text;
+export const COLOR_INFO = colors.info;
+export const COLOR_SUCCESS = colors.success;
+export const COLOR_WARNING = colors.warning;
+export const COLOR_ERROR = colors.error;
