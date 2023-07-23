2bdf1a82c39c464ae2e33f2db9fc753245982918
Anmol Jhamb <talktoanmol@outlook.com>
2023-07-23 13:50:53 +0530

    mark undone, and done

--- server/src/routes/events.ts
+++ server/src/routes/events.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectedRoute, validate } from "../middleware";
-import { createEventSchema } from "../schemas";
+import { createEventSchema, fetchEventSchema } from "../schemas";
 import { BaseProject, ValidationSchema } from "../types";
 import { adminDb } from "../utils";
 
@@ -54,7 +54,59 @@ eventRouter.post(
             const { name, desc } = req.body as BaseProject;
             const docRef = adminDb.collection("events").doc();
             await docRef.set({ name, desc, uid: docRef.id, done: false });
-            return res.status(200).json({ name, desc, uid: docRef.id });
+            return res
+                .status(200)
+                .json({ name, desc, uid: docRef.id, done: false });
+        } catch (e) {
+            next(e);
+        }
+    }
+);
+
+eventRouter.get(
+    "/undo",
+    protectedRoute,
+    validate(fetchEventSchema as unknown as ValidationSchema),
+    async (req, res, next) => {
+        try {
+            const uid = req.query.uid as string;
+            const resp = await adminDb.collection("events").doc(uid).update({
+                done: false,
+            });
+            return res.status(200).json(resp);
+        } catch (e) {
+            next(e);
+        }
+    }
+);
+
+eventRouter.get(
+    "/done",
+    protectedRoute,
+    validate(fetchEventSchema as unknown as ValidationSchema),
+    async (req, res, next) => {
+        try {
+            const uid = req.query.uid as string;
+            const resp = await adminDb.collection("events").doc(uid).update({
+                done: true,
+            });
+            return res.status(200).json(resp);
+        } catch (e) {
+            next(e);
+        }
+    }
+);
+
+eventRouter.delete(
+    "/",
+    protectedRoute,
+    validate(fetchEventSchema as unknown as ValidationSchema),
+    async (req, res, next) => {
+        try {
+            const uid = req.query.uid as string;
+            const resp = await adminDb.collection("events").doc(uid).delete();
+            // todo delete the tasks with projectId as the given id.
+            return res.status(200).json(resp);
         } catch (e) {
             next(e);
         }
