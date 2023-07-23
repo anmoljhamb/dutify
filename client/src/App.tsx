be3f08a7201a05ef71fbff2b09699a52f3e15768
Anmol Jhamb <talktoanmol@outlook.com>
2023-07-23 17:11:11 +0530

    MUI added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material";
+
 const App = () => {
     return (
-        <div className="bg-blac justitfy-center flex h-screen w-screen flex-col">
-            <h1 className="text-4xl uppercase text-black">hello world!</h1>
+        <div className="justitfy-center flex h-screen w-screen flex-col items-center justify-center bg-black">
+            <h1 className="text-4xl uppercase text-white">hello world!</h1>
+            <Button>Login???</Button>
         </div>
     );
 };
